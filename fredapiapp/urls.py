c96724717373e0f27c44b7fc83471d28b27f1cf8
Agnes <agypeter97@gmail.com>
2024-02-06 23:08:28 +0300

    template to display GDP data

--- fredapiapp/urls.py
+++ fredapiapp/urls.py
@@ -5,5 +5,5 @@ from .views import *
 
 urlpatterns = [
     path('', default_view, name='default_view'),
-    path('get_fred_series/<str:series_id>/', get_fred_series, name='get_fred_series'),
+    path('get_fred_series/', get_fred_series, name='get_fred_series'),
 ]
--- fredapiapp/views.py
+++ fredapiapp/views.py
@@ -8,16 +8,18 @@ from django.shortcuts import render
 def default_view(request):
     return HttpResponse("Welcome to the FRED API Django App!")
 
-def get_fred_series(request, series_id='GDP'):
+def get_fred_series(request):
     api_key = config('FRED_API_KEY')
-    fred_api_url = f'https://api.stlouisfed.org/fred/series/observations?series_id={series_id}&api_key={api_key}&file_type=json'
+
+    #endpoint for series observation
+    fred_api_url = f'https://api.stlouisfed.org/fred/series/observations?series_id=GNPCA&api_key={api_key}&file_type=json'
 
     response = requests.get(fred_api_url)
     data = response.json()
 
     context = {
         'data' : data,
-        'series_id' : series_id,
+        # 'series_id' : series_id,
     }
 
     return render (request, 'home.html', context)
