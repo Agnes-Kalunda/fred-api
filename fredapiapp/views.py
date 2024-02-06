1468b2052453e95741e78730f1fddf0aaa356f01
Agnes <agypeter97@gmail.com>
2024-02-06 21:53:45 +0300

    request json data from fred api

--- fredapiapp/views.py
+++ fredapiapp/views.py
@@ -2,18 +2,22 @@
 from django.http import JsonResponse , HttpResponse
 import requests
 from decouple import config
+from django.shortcuts import render
 
 
 def default_view(request):
     return HttpResponse("Welcome to the FRED API Django App!")
 
-def get_fred_series(request, series_id):
-    api_key = config(FRED_API_KEY)
-    fred_api_url = f'https://api.stlouisfed.org/fred/series?series_id=GNPCA&api_key=abcdefghijklmnopqrstuvwxyz123456&file_type=json'
+def get_fred_series(request, series_id='GDP'):
+    api_key = config('FRED_API_KEY')
+    fred_api_url = f'https://api.stlouisfed.org/fred/series/observations?series_id={series_id}&api_key={api_key}&file_type=json'
 
-    try:
-        response = requests.get(fred_api_url)
-        data = response.json()
-        return JsonResponse(data)
-    except Exception as e:
-        return JsonResponse({'error': str(e)})
+    response = requests.get(fred_api_url)
+    data = response.json()
+
+    context = {
+        'data' : data,
+        'series_id' : series_id,
+    }
+
+    return render (request, 'home.html', context)
