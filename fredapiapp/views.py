8e66cd1ebdf13e946c1e149b65ba08729f1e84a9
Agnes <agypeter97@gmail.com>
2024-02-06 23:27:12 +0300

    add series_id parameter

--- fredapiapp/views.py
+++ fredapiapp/views.py
@@ -8,18 +8,18 @@ from django.shortcuts import render
 def default_view(request):
     return HttpResponse("Welcome to the FRED API Django App!")
 
-def get_fred_series(request):
+def get_fred_series(request, series_id='UNRATE'):
     api_key = config('FRED_API_KEY')
 
     #endpoint for series observation
-    fred_api_url = f'https://api.stlouisfed.org/fred/series/observations?series_id=GNPCA&api_key={api_key}&file_type=json'
+    fred_api_url = f'https://api.stlouisfed.org/fred/series/observations?series_id={series_id}&api_key={api_key}&file_type=json'
 
     response = requests.get(fred_api_url)
     data = response.json()
 
     context = {
         'data' : data,
-        # 'series_id' : series_id,
+        'series_id' : series_id,
     }
 
     return render (request, 'home.html', context)
